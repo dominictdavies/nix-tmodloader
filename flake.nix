66a1ce908be039f9ad4b1fb8f316ef402a359fdf
andOrlando <bennettgillig@gmail.com>
2024-12-13 01:51:23 -0500

    removes flake-utils thing

--- flake.nix
+++ flake.nix
@@ -2,13 +2,12 @@
   description = "";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, flake-utils, ... }: 
-  flake-utils.lib.eachDefaultSystem (system: {
+  outputs = { self, ... }: 
+  {
     overlay = final: prev: { tmodloader-server = final.callPackage ./pkgs/default.nix {}; };
     overlays.default = self.overlay;
     nixosModules.default = import ./modules/tmodloader-server;
-  });
+  };
 }
