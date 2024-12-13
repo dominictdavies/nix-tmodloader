4679c04affe147996409c9e453abc7c5fb47d7b5
andOrlando <bennettgillig@gmail.com>
2024-12-13 01:45:43 -0500

    Uses flake-utils

--- flake.nix
+++ flake.nix
@@ -2,12 +2,13 @@
   description = "";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, ... }: 
-  {
+  outputs = { self, flake-utils, ... }: 
+  flake-utils.lib.eachDefaultSystem (system: {
     overlay = final: prev: { tmodloader-server = final.callPackage ./pkgs/default.nix {}; };
     overlays.default = self.overlay;
-    nixosModules.default = import ./modules/tmodloader-server.nix;
-  };
+    nixosModules.default = import ./modules/tmodloader-server;
+  });
 }
